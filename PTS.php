d31f18c65ed1b221daade4eafb484c757e58dd05
Andy <andtyl@gmail.com>
2014-06-04 19:36:48 +0200

    Better number extract

--- PTS.php
+++ PTS.php
@@ -1,27 +1,28 @@
 <?php
-namespace PTSAPI;
+namespace PTS;
 
 class PTS {
     
     /**
-     * Get operator name for phone number
+     * Get operator name for swedish phone number
      *
      * @param string $phone_number phone number
      * @return string Operator name or (bool)false on error
      */
-    public function getOperator($phone_number)
+    public function searchOperatorByNumber($phone_number)
     {
         //Extract number data
-        if (preg_match("~07[0-9]{2}-[0-9]{6}$~", $phone_number)) {
+        if (preg_match("~07[0-9]{2}[\- ][0-9]{6}$~", $phone_number)) {
             $ndc = substr($phone_number, 1, 2);
             $number = substr($phone_number, 3, 1) . substr($phone_number, 5, 6);			
         } elseif (
-            preg_match("~^\+?46(?<ndc>7[0-9]{1})(?<number>[0-9]+)$~", $phone_number, $matches) || 
+            preg_match("~^(\+?46|0)(?<ndc>7[0-9]{1})(?<number>[0-9]+)$~", $phone_number, $matches) ||
             preg_match("~^0?(?<ndc>[0-9]+)[\- ](?<number>[0-9]+)$~", $phone_number, $matches)
         ) {
             $ndc = $matches['ndc'];
             $number = $matches['number'];		
         } else {
+            //Unsupported format :(
             return false;
         }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+Get operator name for swedish phone number via PTS API
+------------------------------------------------------
+
+http://e-tjanster.pts.se/
